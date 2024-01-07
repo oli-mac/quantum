f3a4d9fa71fa9c536866efca1131f38d51c6a698
oli-mac <adumulugeta@gmail.com>
2024-01-06 21:41:36 -0900

    gemini chat

--- lib/components/shortcuts.dart
+++ lib/components/shortcuts.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quantum/pages/chat.dart';
 
 class ShortCuts extends StatelessWidget {
   final String image;
+  final Function? onTap;
   const ShortCuts({
     super.key,
     required this.image,
+    this.onTap,
   });
 
   @override
@@ -15,8 +19,14 @@ class ShortCuts extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
       ),
       padding: const EdgeInsets.all(16),
-      child: Center(
-        child: Image.asset(image, width: 35, height: 35),
+      child: GestureDetector(
+        onTap: () => {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => Chat()))
+        },
+        child: Center(
+          child: Image.asset(image, width: 35, height: 35),
+        ),
       ),
     );
   }
--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:quantum/pages/test.dart';
 
 class Chat extends StatefulWidget {
   const Chat({super.key});
@@ -10,6 +13,112 @@ class Chat extends StatefulWidget {
 class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Overlay(
+        initialEntries: [
+          //! back ground image
+          OverlayEntry(builder: (context) {
+            return Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 18.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        padding: const EdgeInsets.all(14),
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const Test()));
+                          },
+                        ),
+                      ),
+                      //? message
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Quantum",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            "23 Jan 2023",
+                            style: TextStyle(
+                              color: Colors.blue[200],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      //? notification
+
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        padding: const EdgeInsets.all(16),
+                        child: IconButton(
+                          icon: const Icon(Icons.done_all),
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const Test()));
+                          },
+                        ),
+                      ),
+                    ]),
+              ),
+            );
+          }),
+
+          //! main content
+          OverlayEntry(builder: (context) {
+            return Center(
+                child: Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: DraggableScrollableSheet(
+                initialChildSize: 0.95,
+                minChildSize: 0.4,
+                maxChildSize: 0.95,
+                builder: (context, controller) {
+                  return ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: ListView(controller: controller, children: [
+                            const SizedBox(height: 30),
+                          ]),
+                        ),
+                      ));
+                },
+              ),
+            ));
+          }),
+        ],
+      ),
+    );
   }
 }
